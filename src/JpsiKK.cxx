2eee9e6d15c1acc5388770ed3795781e0593446b
Ivan Nikolaev ( shono ) <I.B.Nikolaev@inp.nsk.su>
2014-11-16 18:22:58 +0600

    Fixing compilation error: Second step for EventNavigator

--- src/JpsiKK.cxx
+++ src/JpsiKK.cxx
@@ -887,7 +887,7 @@ StatusCode JpsiKK::execute()
     McParticleVector particles = navigator->getMcParticles(mdcTrk);
     cout << "Retrieved " << particles.size() << " McParticles for for MdcKalTrack # " 
       << mdcTrk->trackId() << " of reconstructed momentum " << mdcTrk->p() << " GeV/c (PID=" 
-      << endl; //mdcTrk->getPidType() << ")" << endreq;
+      << particleProperty() << endl;
     
 
     
