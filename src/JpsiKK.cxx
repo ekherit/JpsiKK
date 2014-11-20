08b4783747e983bb78aae5857288b89234509b7b
Ivan Nikolaev ( shono ) <I.B.Nikolaev@inp.nsk.su>
2014-11-20 14:21:22 +0600

    kinematic fit iteration

--- src/JpsiKK.cxx
+++ src/JpsiKK.cxx
@@ -709,13 +709,13 @@ bool kinematic_fit(int PID, TrackPair_t  & pion_pair, TrackPair_t &  other_pair,
     kmfit->AddTrack(i,vtxfit->wtrk(i));
   }
   HepLorentzVector Pcmf(0.040546,0,0,PSIP_MASS); //initial vector of center of mass frame
-  //kmfit->AddTotalEnergy(0,PSIP_MASS,0,1,2,3);
-  //kmfit->AddFourMomentum(0,  Pcmf);
-  kmfit->AddResonance(0, JPSI_MASS, 2, 3);
-  kmfit->AddResonance(1, PSIP_MASS, 0, 1, 2,3);
+  kmfit->AddTotalEnergy(0,PSIP_MASS,0,1,2,3);
+  kmfit->AddFourMomentum(0,  Pcmf);
+  //kmfit->AddResonance(0, JPSI_MASS, 2, 3);
+  //kmfit->AddResonance(1, PSIP_MASS, 0, 1, 2,3);
   //if(!kmfit->Fit(0)) return false;
   kmfit->Fit(0);
-  kmfit->Fit(1);
+  //kmfit->Fit(1);
   bool oksq = kmfit->Fit();
   if(oksq) 
   {
