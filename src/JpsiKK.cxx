dadc2cd4293711ff3ab426b4d3ecea4dc6e7e123
Ivan Nikolaev ( shono ) <I.B.Nikolaev@inp.nsk.su>
2014-11-21 15:54:43 +0600

    Debugging runtime error

--- src/JpsiKK.cxx
+++ src/JpsiKK.cxx
@@ -1458,7 +1458,7 @@ StatusCode JpsiKK::execute()
   fDedx.tuple->write();
   fTof.tuple->write();
   fNeutral.tuple->write();
-  if(fEvent.run < 0) fMC.tuple->write();
+  //if(fEvent.run < 0) fMC.tuple->write();
   event_write++;
   return StatusCode::SUCCESS;
 }
