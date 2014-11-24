11197a5663591ed395cb8628cf9a3adf9e6eb119
Ivan Nikolaev ( shono ) <I.B.Nikolaev@inp.nsk.su>
2014-11-24 10:43:52 +0600

    Working on make-sim-options

--- share/make-sim-options.py
+++ share/make-sim-options.py
@@ -8,4 +8,18 @@ if len(sys.argv)<4:
     print "Usage: make-sim-options.py <decay_file> <output_prefix> <event_number>"
     exit(1)
 
-print $JPSIKKROOT
+HOME_DIR = os.environ['HOME']
+JPSIKKROOT_DIR = os.environ['JPSIKKROOT']
+SHARE_DIR = os.path.join(JPSIKKROOT_DIR, "share")
+TEMPLATE_DIR = os.path.join(JPSIKKROOT_DIR, "share/template")
+
+TEMPLATE_SIM_FILE = os.path.joint(TEMPLATE_DIR, "simulation.cfg")
+print HOMEDIR, JPSIKKROOT_DIR, TE
+
+DECAY_FILE = os.path.abspath(os.path.join(SHARE_DIR,sys.argv[1]))
+PREFIX = sys.argv[2]
+
+RTRAW_FILE = os.path.abspath(PREFIX+".rtraw")
+DST_FILE = os.path.abspath(PREFIX+".dst")
+ROOT_FILE = os.path.abspath(PREFIX+".root")
+
