173050af87f69ca1f4d916fd238f52bfd0b4f961
Ivan Nikolaev ( shono ) <I.B.Nikolaev@inp.nsk.su>
2014-11-24 10:21:02 +0600

    make sim options file

---
+++ share/make-sim-options.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python 
+#This script create simulation and reconstruction options
+import os
+import sys
+import re
+
+if len(sys.argv)<4:
+    print "Usage: make-sim-options.py <decay_file> <output_prefix> <event_number>"
+    exit(1)
+
+echo $JPSIKKROOT
