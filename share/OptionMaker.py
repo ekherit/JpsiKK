534a1aaa903ee94f64f2f7fb0516853046179c1c
Ivan Nikolaev <i.b.nikolaev@gmail.com>
2014-12-18 12:24:07 +0600

    Create simulation based on dec file

--- share/OptionMaker.py
+++ share/OptionMaker.py
@@ -52,31 +52,77 @@ def make_files_string(files):
 
 
 class OptionMaker:
+    JPSIKKROOT_DIR       = os.path.abspath(os.environ['JPSIKKROOT'])
+    TEMPLATE_DIR         = os.path.join(JPSIKKROOT_DIR, 'share/template')
     dataDir = ""   #folder where data files will be searched
     templateFile=""# the path to the template file
-    targetDir=""   #the folder where options file will be located
+    decayFile=""   #the decay file
+    targetDir="."   #the folder where options file will be located
     jobPrefix=""   #prefix of the job
     fileFilter=""  #dst regex filter
     runFilter=""   #regexp run grouping
     eventNumber=-1 #Event number per job
     runNumber=1    #Run number per job
+    runs=""
     jobNumber=1    #number of jobs
     Seed=1         #the random seed
     fileList=[]
     runMap={}
-    decayFile=""
-
-    def __init__(self, decay_file, jobs_number):
-        self.jobNumber=jobs_number
-        self.decayFile = os.path.realpath(os.path.abspath(decay_file))
+    SelectionMode = True
+    SimulationMode = False
+    Reconstruction = False
 
 
-    def __init__(self, data_dir, file_filter=".+.dst$"):
-        self.dataDir = os.path.abspath(data_dir)
-        self.fileFilter=file_filter
-        self.fileList = filter_file_list(create_file_list(self.dataDir), self.fileFilter)
+    def __init__(self, options, args):
+        self.decayFile = os.path.realpath(os.path.abspath(args[1]))
+        self.dataDir   = os.path.realpath(os.path.abspath(args[1]))
+        self.eventNumber=options.event_number
+        self.jobNumber=int(options.job_number)
+        self.jobPrefix=options.job_prefix
+        self.runs=options.runs
 
-    #def group(self, run_filter=".*run_(\d\d\d\d\d\d\d).*.dst", run_number=1):
+        if len(args) < 1:
+            print 'Specify the action: "sel",  "sim",  "rec"'
+            sys.exit(1)
+
+        if(args[0] == "selection" or args[0]=="sel"):
+            self.SelectionMode=True
+            self.fileFilter=file_filter
+            self.fileList = filter_file_list(create_file_list(self.dataDir), self.fileFilter)
+            self.templateFile = "selection.cfg"
+
+        if args[0] == "simulation" or args[0] == "sim":
+            self.SimulationMode = True
+            self.templateFile = "simulation.cfg"
+            print "Making simulation config files."
+
+        if args[0] == "reconstruction" or args[0] == "rec":
+            self.ReconstructionMode = True
+            self.templateFile = "reconstruction.cfg"
+            print "Making reconstruction config files."
+
+
+        if not os.path.exists(self.templateFile):
+            self.templateFile = os.path.join(self.TEMPLATE_DIR, self.templateFile)
+            print self.templateFile
+        else:
+            self.templateFile = os.path.realpath(os.path.abspath(self.templateFile))
+            print self.templateFile
+        if not os.path.exists(self.templateFile):
+            print 'Template file ',  options.template_file,  ' doest exists'
+            sys.exit(1)
+        print "Setup template file: ",  self.templateFile
+	if len(args) >= 2:
+		self.targetDir = os.path.realpath(os.path.abspath(args[2]))
+        if not os.path.exists(self.targetDir):
+            os.mkdir(self.targetDir)
+        else:
+            if not options.force:
+                print "Target dir ",  self.targetDir,  " already exists, exiting"
+                sys.exit(1)
+
+
+
     def group(self, run_filter=".*run.*(\d{4,7})[^\d]+.*.dst", run_number=1):
         self.runFilter=run_filter
         self.runNumber = run_number
@@ -93,10 +139,16 @@ class OptionMaker:
             print "Target dir ",  self.targetDir,  " already exists, exiting"
             sys.exit(1)
 
+    def make(self):
+        if self.SimulationMode:
+            self.make_sim()
 
-    def make(self, template_file, target_dir, job_prefix):
-        self.jobPrefix = job_prefix
-        setup_template_file(template_file, target_dir)
+
+
+
+    def make_sel(self, template_file, target_dir, job_prefix):
+        #self.jobPrefix = job_prefix
+        #self.setup_template_file(template_file, target_dir)
 
         for run, files in self.runMap.items():
             #define input and output files in joboptions
@@ -117,16 +169,14 @@ class OptionMaker:
             source_file.close()
             target_file.close()
 
-    def make_sim(self, template_file, target_dir, job_prefix):
-        self.jobPrefix = job_prefix
-        setup_template_file(template_file, target_dir)
+    def make_sim(self):
         for job in range(0, self.jobNumber):
             #define input and output files in joboptions
             TemplateOutputFile = os.path.abspath(os.path.join(self.targetDir,"%s-%07d.rtraw" % (self.jobPrefix,self.jobNumber)))
             TemplateInputFile =  self.decayFile 
             #define the name of cfg file
-            target_file_name = os.path.join(self.targetDir,"%s-sim-%07d.cfg" % (self.jobPrefix, self.jobNumber))
-            target_file_name_rec = os.path.join(self.targetDir,"%s-rec-%07d.cfg" % (self.jobPrefix, self.jobNumber))
+            target_file_name = os.path.join(self.targetDir,"%s-sim-%07d.cfg" % (self.jobPrefix, job))
+            target_file_name_rec = os.path.join(self.targetDir,"%s-rec-%07d.cfg" % (self.jobPrefix, job))
             source_file = open(self.templateFile, 'r')
             target_file = open(target_file_name, 'w')
             TemplateRandomSeed = str(random.randint(0,2**32))
@@ -134,8 +184,8 @@ class OptionMaker:
                 line = re.sub("TEMPLATE_INPUT_FILE", TemplateInputFile, line)
                 line = re.sub("TEMPLATE_OUTPUT_FILE",TemplateOutputFile, line)
                 line = re.sub("TEMPLATE_RANDOM_SEED",TemplateRandomSeed, line)
-                line = re.sub("TEMPLATE_EVENT_NUMBER",self.eventNumber, line)
-                line = re.sub("TEMPLATE_RUN_NUMBER",-run, line)
+                line = re.sub("TEMPLATE_EVENT_NUMBER",str(self.eventNumber), line)
+                line = re.sub("TEMPLATE_RUN_NUMBER",self.runs, line)
                 target_file.write(line)
             source_file.close()
             target_file.close()
--- share/mkopt.py
+++ share/mkopt.py
@@ -10,114 +10,29 @@ from optparse import OptionParser
 opt = OptionParser()
 
 
-opt.add_option("-t", "--template", dest="template_file",
+opt.add_option("-t", "--template_file", dest="template_file", default="selection.cfg", 
                   help="Template configuration file", metavar="FILE")
 
-opt.add_option("-p", "--prefix", dest="job_prefix",
+opt.add_option("-p", "--job_prefix", dest="job_prefix",
                   help="Prefix for the pbs job", default="test")
 
-opt.add_option("-j", "--jobs", dest="jobs_number",
+opt.add_option("-J", "--job_number", dest="job_number",default=1, 
                   help="The jobs number")
 
-opt.add_option("-N", "--events", dest="event_number","default=100", 
-                  help="Event number")
+opt.add_option("-N", "--event_number", dest="event_number",default=100, 
+                  help="Event number per job")
+
+opt.add_option("-r", "--run", dest="runs",default="-8093", 
+                  help="Number of run in simulation")
+
+opt.add_option("-f", "--force", action="store_true", dest="force", 
+                  help="Force ")
 
 
 (options, args) = opt.parse_args()
 
-JPSIKKROOT_DIR = os.path.abspath(os.environ['JPSIKKROOT'])
-TEMPLATE_DIR = os.path.join(JPSIKKROOT_DIR, 'share/template')
-TEMPLATE_DECAY_FILE=os.path.join(TEMPLATE_DIR, 'decay.dec')
-TEMPLATE_FILE = "selection.cfg"
 
+optMaker = OptionMaker(options, args)
+optMaker.make();
 
-SelectionMode = False
-SimulationMode = False
-ReconstructionMode = False
-
-if len(args) < 1:
-	print 'Specify the action: "sel",  "sim",  "rec"'
-	sys.exit(1)
-
-if args[0] == "selection" or args[0] == "sel":
-	SelectionMode = True
-	TEMPLATE_FILE = "selection.cfg"
-	print "Making selection config files."
-
-if args[0] == "simulation" or args[0] == "sim":
-	SimulationMode = True
-	TEMPLATE_FILE = "simulation.cfg"
-	print "Making simulation config files."
-
-if args[0] == "reconstruction" or args[0] == "rec":
-	ReconstructionMode = True
-	TEMPLATE_FILE = "reconstruction.cfg"
-	print "Making reconstruction config files."
-
-
-#configure template config file
-if not os.path.exists(TEMPLATE_FILE):
-	TEMPLATE_FILE = os.path.join(TEMPLATE_DIR, TEMPLATE_FILE)
-else:
-	TEMPLATE_FILE = os.path.realpath(os.path.abspath(TEMPLATE_FILE))
-if not os.path.exists(TEMPLATE_FILE):
-	print 'Template file ',  TEMPLATE_FILE,  ' doest exists'
-	sys.exit(1)
-
-print "Setup template file: ",  TEMPLATE_FILE
-
-if SimulationMode:
-	if len(args) < 3:
-		print 'Specify decay file of the simulation and event number'
-		sys.exit(1)
-	TEMPLATE_DECAY_FILE = args[1]
-        TEMPLATE_EVENT_NUMBER=int(args[2])
-	if not os.path.exists(TEMPLATE_DECAY_FILE):
-		TEMPLATE_DECAY_FILE = os.path.join(TEMPLATE_DIR, TEMPLATE_DECAY_FILE)
-		if not os.path.exists(TEMPLATE_DECAY_FILE):
-			print 'Decay file ',  TEMPLATE_DECAY_FILE,  ' doest exists'
-			sys.exit(1)
-	else:
-		TEMPLATE_DECAY_FILE = os.path.realpath(os.path.abspath(TEMPLATE_DECAY_FILE))
-	print "Use decay file: ",  TEMPLATE_DECAY_FILE
-        optMaker = OptionMaker(TEMPLATE_DECAY_FILE, TEMPLATE_EVENT_NUMBER, options.jobs_number)
-	TARGET_DIR="."
-	if len(args) == 3:
-		TARGET_DIR = os.path.realpath(os.path.abspath(args[2]))
-        optMaker.make_sim(TEMPLATE_FILE, TARGET_DIR, options.job_prefix)
-
-
-
-if SelectionMode:
-	if len(args) < 2:
-		print 'specify source dir with dst files or dst file'
-		sys.exit(1)
-	DATA_SOURCE_DIR = args[1]
-	if not os.path.exists(DATA_SOURCE_DIR):
-			print 'Source data dir or dst file ',  DATA_SOURCE_DIR,  ' does not exists'
-	if os.path.isdir(DATA_SOURCE_DIR):
-		DATA_SOURCE_DIR = os.path.realpath(DATA_SOURCE_DIR)
-	TARGET_DIR="."
-	if len(args) == 3:
-		TARGET_DIR = os.path.realpath(os.path.abspath(args[2]))
-	print "TARGET_DIR =",  TARGET_DIR
-	optMaker = OptionMaker(DATA_SOURCE_DIR)
-	print "Found ",  len(optMaker.fileList),  " *.dst files"
-	optMaker.group()
-	print "Found ",  len(optMaker.runMap), " runs,  creating joboptions..."
-	optMaker.make(TEMPLATE_FILE,TARGET_DIR,options.job_prefix)
-
-	
-#if( args[1] == "simulation" || args[1] == "sim"):
-#	print "Making simulation config files"
-#
-#if( args[1] == "reconstruction" || args[1] == "rec"):
-#	print "Making reconstruction config files"
-
-#TEMPLATE_FILE = os.path.join(TEMPLATE_DIR, 'selection.cfg')
-#
-#print options.selection
-#print args
-#DATA_SOURCE_DIR = os.path.realpath(os.path.abspath(args[0]))
-#
 ##optMaker.group(".*run_(\d\d\d\d\d\d\d).*.dst")
