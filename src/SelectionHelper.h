ea3db72790cba23737aac74e98ced7e2302ab7ec
Ivan Nikolaev <i.b.nikolaev@gmail.com>
2015-10-21 20:11:43 +0600

    debugging selectin

--- src/SelectionHelper.h
+++ src/SelectionHelper.h
@@ -320,7 +320,7 @@ struct SelectionHelper_t
 		passKinPid(cfg);
 		//electron pass must be after kinpid
 		//we must know the channel
-		passElectrons(cfg);
+		if(pass_kinematic) passElectrons(cfg);
 		pass = pass_kinematic && pass_pid && pass_electron;
 		clog << "good_kinematic_fit: " << good_kinematic_fit << "  kin_chi2 = " << kin_chi2 << " " ;
 		clog << "pass_pid: " << pass_pid << "  pid_chi2 = " <<  pid_chi2[channel] << " " ;
