640599e30569f1b288aaeeb7c65b8bb200367f07
Ivan Nikolaev <i.b.nikolaev@gmail.com>
2015-10-22 12:00:46 +0600

    remove debug print...

--- src/SelectionHelper.h
+++ src/SelectionHelper.h
@@ -325,9 +325,9 @@ struct SelectionHelper_t
 		if(!pass_kinematic) return false;
 		passElectrons(cfg);
 		pass = pass_kinematic && pass_pid && pass_electron;
-		clog << "good_kinematic_fit: " << good_kinematic_fit << "  kin_chi2 = " << kin_chi2 << " " ;
-		clog << "pass_pid: " << pass_pid << "  pid_chi2 = " <<  pid_chi2[channel] << " " ;
-		clog << "pass_electron: " << pass_electron << endl;
+		//clog << "good_kinematic_fit: " << good_kinematic_fit << "  kin_chi2 = " << kin_chi2 << " " ;
+		//clog << "pass_pid: " << pass_pid << "  pid_chi2 = " <<  pid_chi2[channel] << " " ;
+		//clog << "pass_electron: " << pass_electron << endl;
 		return pass;
 	}
 
