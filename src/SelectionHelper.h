04509dc9ea00268ae111b37d285678595d2c67bf
Ivan Nikolaev <i.b.nikolaev@gmail.com>
2015-11-02 13:04:30 +0600

    proton preselected hypo

--- src/SelectionHelper.h
+++ src/SelectionHelper.h
@@ -227,7 +227,7 @@ struct SelectionHelper_t
 		pid_list.push_back(ID_MUON);
 		//pid_list.push_back(ID_ELECTRON);
 		//pid_list.push_back(ID_PROTON);
-		channel = ID_ELECTRON; //preselect some hypotesa
+		channel = ID_PROTON; //preselect some hypotesa
 		for(std::list<int>::iterator pid = pid_list.begin(); pid!=pid_list.end() ; pid++)
 		{
 			if(KF[*pid].chi2 < KF[channel].chi2)
