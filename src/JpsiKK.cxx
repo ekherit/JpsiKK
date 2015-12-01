b5154d89b8afbeb5f250db92d60de31afb49dcc6
Ivan Nikolaev <i.b.nikolaev@gmail.com>
2015-12-01 13:45:00 +0600

    Fixing compilation bug

--- src/JpsiKK.cxx
+++ src/JpsiKK.cxx
@@ -309,21 +309,21 @@ StatusCode JpsiKK::execute()
   if(neg)
   {
     sh_list.push_back(SelectionHelper_t);
-    SelectionHelper_t & sh = shl.back();
+    SelectionHelper_t & sh = sh_list.back();
 		sh.kinfit(pion_pair, other_negative_tracks.front());
 		sh.totalPass(cfg.PASS_KIN_PID_CUT);
   }
   if(pos)
   {
     sh_list.push_back(SelectionHelper_t);
-    SelectionHelper_t & sh = shl.back();
+    SelectionHelper_t & sh = sh_list.back();
 		sh.kinfit(pion_pair, other_positive_tracks.front());
 		sh.totalPass(cfg.PASS_KIN_PID_CUT);
   }
   if(pos && neg)
   {
     sh_list.push_back(SelectionHelper_t);
-    SelectionHelper_t & sh = shl.back();
+    SelectionHelper_t & sh = sh_list.back();
     sh.kinfit(pion_pair, other_negative_tracks.front(), other_positive_tracks.front());
 		sh.totalPass(cfg.PASS_KIN_PID_CUT);
   }
