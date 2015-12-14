69b3d4289f0f661a5eaf312a26e0db6fb265ccdb
Ivan Nikolaev ( shono ) <I.B.Nikolaev@inp.nsk.su>
2015-12-14 12:45:00 +0600

    Fixing compilation bug

--- src/MyPid.h
+++ src/MyPid.h
@@ -21,9 +21,9 @@
 #include "EvtRecEvent/EvtRecTrack.h"
 #include "DstEvent/TofHitStatus.h"
 
-SmartRefVector<RecTofTrack>::iterator  getTofTrk(EvtRecTrackIterator itTrk, bool & isTofValid)
+SmartRefVector<RecTofTrack>::iterator  getTofTrk(EvtRecTrack * track, bool & isTofValid)
 {
-  SmartRefVector<RecTofTrack> tofTrkCol = (*itTrk)->tofTrack();
+  SmartRefVector<RecTofTrack> tofTrkCol = track->tofTrack();
   SmartRefVector<RecTofTrack>::iterator tofTrk = tofTrkCol.begin();
   TofHitStatus *hitst = new TofHitStatus;
   std::vector<int> tofecount;
@@ -48,16 +48,16 @@ SmartRefVector<RecTofTrack>::iterator  getTofTrk(EvtRecTrackIterator itTrk, bool
 
 
 
-vector<double> get_chi2(EvtRecTrackIterator & itTrk)
+vector<double> get_chi2(EvtRecTrack * Trk)
 {
   vector<double> chi2(5,99999);
-  if(!(*itTrk)->isMdcTrackValid()) return chi2;
-  if(!(*itTrk)->isMdcDedxValid())  return chi2;
+  if(!Trk->isMdcTrackValid()) return chi2;
+  if(!Trk->isMdcDedxValid())  return chi2;
   for(int i=0;i<5;i++) chi2[i]=0;
-  RecMdcTrack * mdcTrk  = (*itTrk)->mdcTrack();
+  RecMdcTrack * mdcTrk  = Trk->mdcTrack();
 
   //dedx information
-  RecMdcDedx  * dedxTrk = (*itTrk)->mdcDedx();
+  RecMdcDedx  * dedxTrk = Trk->mdcDedx();
   chi2[ID_KAON]     +=   sq(dedxTrk->chiK());
   chi2[ID_MUON]     +=   sq(dedxTrk->chiMu());
   chi2[ID_ELECTRON] +=   sq(dedxTrk->chiE());
@@ -65,9 +65,9 @@ vector<double> get_chi2(EvtRecTrackIterator & itTrk)
   chi2[ID_PROTON]   +=   sq(dedxTrk->chiP());
 
   //tof information
-  if(!(*itTrk)->isTofTrackValid()) return chi2;
+  if(!Trk->isTofTrackValid()) return chi2;
   bool isTofValid=false;
-  SmartRefVector<RecTofTrack>::iterator tofTrk = getTofTrk(itTrk, isTofValid);
+  SmartRefVector<RecTofTrack>::iterator tofTrk = getTofTrk(Trk, isTofValid);
   if(isTofValid)
   {
     double t = (*tofTrk)->tof();  //flight time
@@ -84,22 +84,22 @@ vector<double> get_chi2(EvtRecTrackIterator & itTrk)
 
 vector< vector<double> > get_chi2(TrackPair_t & pion_pair, TrackPair_t & kaon_pair)
 {
-  EvtRecTrackIterator  itTrk[4] = {pion_pair.first, pion_pair.second, kaon_pair.first, kaon_pair.second};
+  EvtRecTrack *  Trk[4] = {pion_pair.first, pion_pair.second, kaon_pair.first, kaon_pair.second};
   vector< vector<double> > chi2(4);
   for(int track=0;track<chi2.size();track++)
   {
-    chi2[track] = get_chi2(itTrk[track]);
+    chi2[track] = get_chi2(Trk[track]);
   }
   return chi2;
 }
 
-vector<double> get_chi2(std::pair<EvtRecTrackIterator, EvtRecTrackIterator> & pair)
+vector<double> get_chi2(TrackPair_t & pair)
 {
-  EvtRecTrackIterator  itTrk[2] = {pair.first, pair.second};
+  EvtRecTrack*  Trk[2] = {pair.first, pair.second};
   vector<double> chi2(5,0);
   for(int track=0;track<2;track++)
   {
-    vector<double> tmp_chi2=get_chi2(itTrk[track]);
+    vector<double> tmp_chi2=get_chi2(Trk[track]);
     for(int i=0;i<5;i++)
     {
       chi2[i]  += tmp_chi2[i]; 
