a295556d3e93f0ed073e36f2bfaa9ea85c672385
Ivan Nikolaev <i.b.nikolaev@gmail.com>
2015-12-29 13:26:10 +0600

    fix config bug (forgot 3.65 energy)

--- bin/mkcnt.sh
+++ bin/mkcnt.sh
@@ -1,2 +1,2 @@
-$JPSIKKROOT/share/mkopt.py sel  /bes3fs/offline/data/664-1/3650/dst  cnt3650   -p "cnt" $$
+$JPSIKKROOT/share/mkopt.py sel  /bes3fs/offline/data/664-1/3650/dst  cnt3650   -p "cnt" -E 3.65 $$
 $JPSIKKROOT/share/mkpbs.py -p cnt -n 5 cnt3650/
